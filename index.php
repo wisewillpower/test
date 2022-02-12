6050614ca449e665d0e41c22fb9e1080f8734213
wisewillpower <493531514@qq.com>
2022-02-12 23:49:28 +0800

    m

--- index.php
+++ index.php
@@ -3,5 +3,7 @@ echo "<h1><a href='#' >自动部署的开始...</a></h1>";
 echo "<h2>试试</h2>";
 echo "<hr/>";
 echo "<h3>syt0592@sohu.com</h3>";
+$path = "D:\WebRoot\cs";
+shell_exec("d: >d:/1.txt && cd {$path} > d:/2.txt && git reset --hard origin/master >d:/3.txt && git clean -f > d:/4.txt && git pull d:/5.txt")
 ?>
 
