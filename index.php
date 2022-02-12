b116b617cd2cab89852b53d1c83a8af094f427a8
wisewillpower <493531514@qq.com>
2022-02-13 01:07:37 +0800

    m

--- index.php
+++ index.php
@@ -3,7 +3,8 @@ echo "<h1><a href='#' >自动部署的开始...</a></h1>";
 echo "<h2>试试</h2>";
 echo "<hr/>";
 echo "<h3>syt0592@sohu.com</h3>";
-echo "<h4>.网络时代.</h4>"
-echo "<h5>.DNA.</h5>"
+echo "<h4>.网络时代.</h4>";
+echo "<h5>.DNA.</h5>";
+echo "<h6>.h6.</h6>";
 ?>
 
